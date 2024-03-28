313cf33ebdaf4e06f3433ba5a36385559ab702aa
Ad3an <144132964+Ad3an@users.noreply.github.com>
2024-03-28 12:45:33 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function realPageAnimation() {
+  gsap.to(".nature", {
+    scrollTrigger: {
+      trigger: ".nature-section ",
+      start: "top top",
+      end: "bottom top",
+      scrub: 1,
+      pin: true,
+    },
+    xPercent: -200,
+    ease: Power4,
+  });
+}
+function heroAnimation() {
+  gsap.to(".hero-section .img img", {
+    scrollTrigger: {
+      trigger: ".hero-section",
+      pin: true,
+      scrub: 1,
+    },
+    scale: 1.1,
+    ease: Power3,
+  });
+}
+function loco(){
+  (function () {
+    const locomotiveScroll = new LocomotiveScroll();
+    mobile:{smooth:true};
+    tablet:{
+      smooth:true;
+    breakpoint:0}
+})();
+}
+
+heroAnimation();
+realPageAnimation();
+loco()
